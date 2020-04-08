6cc2d8e8dce175fdfa94a545a6d565949af594d9
Pontus <Pontus@Pihl.nu>
2020-04-08 14:02:05 +0200

    added main statemachine and comments

--- Mower/CommandHandler.cpp
+++ Mower/CommandHandler.cpp
@@ -6,10 +6,17 @@ CommandHandler::CommandHandler()
 {
 	
 }
+void CommandHandler::init()
+{
+}
+
+
+
 
 
 CommandHandler::~CommandHandler()
 {	delete instance;}
+
 
 CommandHandler * CommandHandler::getInstance()
 {
@@ -18,22 +25,118 @@ CommandHandler * CommandHandler::getInstance()
 	return instance;
 }
 
+void CommandHandler::run()
+{
+	enum state_e
+	{
+		idle,
+		doSequence,
+		callback,
+	};
+	static state_e state = idle;
+
+
+	switch (state)
+	{
+	case idle://wait until there is a sequense to run
+		if (!commandQueue.isEmpty())
+			state = doSequence;
+		break;
+	case doSequence:// runs the sequense and waits until it's done
+		if (runSequence())
+			state = callback;
+		break;
+	case callback:// calls thr callback function (if there is any) and removes the sequense from the queue
+		if (commandQueue.front().callback != nullptr)
+			commandQueue.front().callback();
+		commandQueue.pop();
+		state = idle;
+		break;
+	default:
+		break;
+	}
+}
+bool CommandHandler::runSequence()
+{
+	enum state_s
+	{
+		load,
+		wait,
+		send,
+		check,
+	};
+	static state_s state = send;
+	static cmdSequense* sequence = nullptr;
+
+	switch (state)
+	{
+	case load:// loads the front sequense from the queue
+		if (commandQueue.isEmpty())
+			return true;
+		sequence = &commandQueue.front();
+		state = wait;
+		break;
+	case wait:// waits to run the sequense until the engine is ready
+		//TODO implement ready function in engine to check here
+		//if (ready()) state = send;
+		break;
+	case send:// sends the front cmd in the sequense to the engine and removes it from the queue
+		sendCmdToEngine(sequence->sequense.front());
+		sequence->sequense.pop();
+		state = check;
+		break;
+	case check://checks if end of sequens and returns trur if it is.
+		if (sequence->sequense.isEmpty())
+		{
+			state = load;
+			return true;
+		}
+		else state = send;
+		break;
+	default:
+		break;
+	}
+
+	return false;
+}
+
+
 void CommandHandler::addCommand(cmd command, void(*callback)(void))
-{	commandQueue.push_back(cmdSequense{ vector<cmd>{command},callback});}
+{
+	QueueArray<cmd> cmdQueue;
+	cmdQueue.push(command);
+	commandQueue.push(cmdSequense {cmdQueue, callback});
+}
 
 void CommandHandler::addCommand(cmd command[],int size, void(*callback)(void))
 {
 	cmdSequense sequense;
 	for (int i = 0; i < size; i++)
 	{
-		sequense.sequense.push_back(command[i]);
+		sequense.sequense.push(command[i]);
 	}
 	sequense.callback = callback;
-	commandQueue.push_back(sequense);
+	commandQueue.push(sequense);
 }
 
 void CommandHandler::addCommand(cmdSequense commandSeq)
-{	commandQueue.push_back(commandSeq);}
+{	commandQueue.push(commandSeq);}
 
 void CommandHandler::clear()
-{	commandQueue.clear();}
+{	
+	while (!commandQueue.isEmpty())
+		commandQueue.pop();
+}
+
+void CommandHandler::stopEngine()
+{
+
+	//TODO implement when engine is ready 
+}
+
+
+
+void CommandHandler::sendCmdToEngine(cmd)
+{
+	//TODO implement when engine is ready
+}
--- Mower/CommandHandler.h
+++ Mower/CommandHandler.h
@@ -1,35 +1,57 @@
 #pragma once
 
+#include <stdlib.h>
 #include <Arduino.h>
+#include <QueueArray.h>
+
+using namespace std;
 
 class CommandHandler
 {
 public:
 
-	struct cmd
+	struct cmd//TODO move this struct to engine class
 	{
-		int speed;
+		/*int speed;
 		int turnRaduis;
-		int time_ms;
+		int time_ms;*/
 	};
+
 	struct cmdSequense
-	{
-		
+	{		
+		QueueArray<cmd> sequense;
+		void(*callback)(void);
 	};
 
 	~CommandHandler();
-	CommandHandler* getInstance();
+	//get singelton instance
+	static CommandHandler* getInstance();
+	//initilize the class. must be called before any other calls
+	void init();
 
+	//run the main state machine
 	void run();
-	void addCommand(cmd);
-	void addCommand(cmd[]);
+
+	//adds an engine command at the back if the queue
+	void addCommand(cmd,void(*callback)(void) = nullptr);
+	void addCommand(cmd[],int, void(*callback)(void) = nullptr);
 	void addCommand(cmdSequense);
+
+	//cleares the entire command queue but dose NOT stop the engine
 	void clear();
+	//cleres the command queue and immediately stops the engine
 	void stopEngine();
 
 private:
 	CommandHandler();
+	
+	//sends a command to engine
+	void sendCmdToEngine(cmd);
+	//sud state machine to run a singilar cmd sequense, returns true when done.
+	bool runSequence();
 
 	static CommandHandler* instance;
-};
+	
+	QueueArray<cmdSequense> commandQueue;
 
+};
