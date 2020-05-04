b3a428f440d0407b1e1a5a88b4d2b450df3753b0
jimsan123 <jimhult@hotmail.se>
2020-05-04 13:47:00 +0200

    LAD-125 Functionallity of ManualDriver

--- Mower/ManualDriver.h
+++ Mower/ManualDriver.h
@@ -1,6 +1,24 @@
 #pragma once
+//#include "EngineModule.h
+#include "CommandHandler.h"
 
-class ManualDriver
+class ManualDriver : public AbsDriver
 {
-  
+public:
+
+	ManualDriver();
+
+	void init();
+
+	void run();
+protected:
+
+	void sendCMD(int, int);
+
+	void listener(int[], int);
+private:
+
+	bool sensorOn = false;
+
+	//SensorController* sensorInstance;
 };
