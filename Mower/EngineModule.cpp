0bedcfec6dbf489e573e2f5d88166784801afb69
Pontus <Pontus@Pihl.nu>
2020-05-06 11:27:43 +0200

    added setSpeed and setTurn functionality to engineModule

--- Mower/EngineModule.cpp
+++ Mower/EngineModule.cpp
@@ -11,11 +11,47 @@ EngineModule::EngineModule(int slot1, int slot2)
 
 void EngineModule::setCommand(cmd command)
 {
-	current_command.speed = command.speed;
-	current_command.turnRadius = command.turnRadius;
-	current_command.time_ms = command.time_ms;
-
+	//current_command.speed = command.speed;
+	//current_command.turnRadius = command.turnRadius;
+	//current_command.time_ms = command.time_ms;
+	current_command = command;
 	_ready = false;
+}
+
+void EngineModule::setTurn(int value)
+{
+	//normalize value to [-255,255]
+	value = constrain(value, -100, 100);
+	value = (value / 100.0f) * 255;
+
+	//get the current pwm for each wheel
+	int leftPWN = Wheel_Left->getCurPwm();
+	int rightPWM = Wheel_Right->getCurPwm();
+
+	//uptade difference between the wheels to match the new turn
+	int diff = leftPWN - rightPWM;
+	leftPWN += (value - diff) / 2;
+	rightPWM -= (value - diff) / 2;
+
+	//set the new pwm
+	Wheel_Left->setMotorPwm(leftPWN);
+	Wheel_Right->setMotorPwm(rightPWM);
+}
+
+void EngineModule::setSpeed(int value)
+{
+	//get the current difference between the whels
+	int turn = Wheel_Left->getCurPwm() - Wheel_Right->getCurPwm();
+	//constraints the speed so that turn difference will remain the same
+	int speed = constrain(value, -255 + (turn / 2), 255 - (turn / 2));
+
+	//gets the current wheel pwm and updates it
+	int leftPWM = Wheel_Left->getCurPwm() + speed;
+	int rightPWM = Wheel_Right->getCurPwm() + speed;
+
+	//set the new pwm
+	Wheel_Left->setMotorPwm(leftPWM);
+	Wheel_Right->setMotorPwm(rightPWM);
 }
 
 bool EngineModule::isReady()
@@ -37,6 +73,13 @@ void EngineModule::run()
 	static state_s state = idle;
 	static unsigned long startWait = 0;
 
+	static cmd oldCmd = cmd{0,0,0};
+	if (!(current_command == oldCmd))
+	{
+		state = update;
+		oldCmd = current_command;
+	}
+
 	switch (state)
 	{
 	case idle:
@@ -44,11 +87,20 @@ void EngineModule::run()
 		{
 			state = update;
 		}
-		else
+		else if (current_command.time_ms >= 0)
 			stopp();
 		break;
 	case update:
 		execute_command(&current_command);
+
+		//if the time is negative, the cmd will run without a timelimit
+		if (current_command.time_ms < 0)
+		{
+			_ready = true;
+			state = idle;
+			break;
+		}
+
 		startWait = millis();
 		state = wait;
 		break;
@@ -57,7 +109,6 @@ void EngineModule::run()
 		{
 			_ready = true;
 			state = idle;
-
 		}
 		break;
 	default:
