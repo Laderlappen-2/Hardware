fbe638bfa91fe0cc7c70c3cebac40a0876dea9b7
Pontus <Pontus@Pihl.nu>
2020-05-11 15:30:21 +0200

    connected btController, commandHandler, and driver modules in main

--- Mower/Mower.ino
+++ Mower/Mower.ino
@@ -3,77 +3,43 @@
 #include "CommandHandler.h"
 #include "BluetoothController.h"
 
+#include "MultiDriver.h"
+#include "ManualDriver.h"
+#include "AutoDriver.h"
+
 CommandHandler *commandHandler;
 BluetoothController *btController = BluetoothController::getInstance();
-AppInstructions* appInstructions = AppInstructions::getInstance();
-byte byteArr[3] = {97,98,99};
+MultiDriver *driver;
 
-void rxListner(int data[], int size)
+enum driveType_e
 {
-	Serial.println("IN rxListner(): Recived data: ");
-	for (int i = 0; i < size; i++)
-	{
-		Serial.print(data[i]);
-		Serial.print(", ");
-	}
-  Serial.print("\n");
-}
+	manual = 'm',
+	autonomous = 'a',
+};
 
-void setTestSequence()
-{
-	//Serial.println("Adding cmd Sequence");
-	//								speed,	turn,		ms
-	//EngineModule::cmd sequence[] = {{appInstructions->getVelocity(),	appInstructions->getTurn(),	1000}};
-
-  EngineModule::cmd sequence[] = {{appInstructions->getVelocity(), appInstructions->getTurn(), 500}};
-	commandHandler->addCommand(sequence, 1, setTestSequence);
-}
-
-//TestCode for bluetooth
-void checkBluetooth()
-{
-  while(Serial.available() > 0)
-  {
-    delay(10);
-    char currentChar = Serial.read();
-    Serial.println(currentChar);
-
-    if(currentChar == '@')
-	    setTestSequence();
-  }
-
-}
+void switchToAutoListner(int *data, int size)
+{	driver->selectDriver(String(autonomous));}
+void switchToManuelListner(int *data, int size)
+{	driver->selectDriver(String(manual));}
 
 void setup()
 {
-  Serial.begin(115200);
-	//btController->init(115200);
+	Serial.begin(115200);
+	btController->addReciveListner(BluetoothController::reciveType_e::atomatic, switchToAutoListner);
+	btController->addReciveListner(BluetoothController::reciveType_e::manuall, switchToManuelListner);
 
-	//commandHandler = CommandHandler::getInstance();
-	commandHandler = new CommandHandler();
+	commandHandler = CommandHandler::getInstance();
 	commandHandler->init(SLOT1,SLOT2);
-	//setTestSequence();
 
-	btController->addReciveListner(BluetoothController::reciveType_e::atomatic, rxListner);
-	btController->addReciveListner(BluetoothController::reciveType_e::drive, rxListner);
-	btController->addReciveListner(BluetoothController::reciveType_e::honk, rxListner);
-	btController->addReciveListner(BluetoothController::reciveType_e::ligh, rxListner);
-	btController->addReciveListner(BluetoothController::reciveType_e::manuall, rxListner);
-	btController->addReciveListner(BluetoothController::reciveType_e::turnOff, rxListner);
+	driver = new MultiDriver();
+	driver->init();
+	driver->addDriver(new ManualDriver(), (String)driveType_e::manual);
+	driver->addDriver(new AutoDriver(), (String)driveType_e::autonomous);
 }
 
 void loop()
 {
 	commandHandler->run();
-  btController->run();
-  //btController->run();
-	//btController->readBluetooth();
-
-  /*if(appInstructions->getInstructionsAvailable())
-  {
-    setTestSequence();
-    appInstructions->setInstructionsAvailable(false);
-    Serial.write(appInstructions->getVelocity());
-  }*/
-  
+	btController->run();
+	driver->run();
 }
